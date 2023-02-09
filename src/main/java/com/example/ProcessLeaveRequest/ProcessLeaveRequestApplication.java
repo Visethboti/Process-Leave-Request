05b5fa9df06960955bb682ffe6e9976fcf8c596e
Visethboti <visethboti@gmail.com>
2023-02-09 08:33:26 +0700

    Experimenting with sprinng security plugin for keycloak

--- src/main/java/com/example/ProcessLeaveRequest/ProcessLeaveRequestApplication.java
+++ src/main/java/com/example/ProcessLeaveRequest/ProcessLeaveRequestApplication.java
@@ -2,6 +2,9 @@ package com.example.ProcessLeaveRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
@@ -15,4 +18,9 @@ public class ProcessLeaveRequestApplication {
 		SpringApplication.run(ProcessLeaveRequestApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		// Do any additional configuration here
+		return builder.build();
+	}
 }

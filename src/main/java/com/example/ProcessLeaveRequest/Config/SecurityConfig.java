dd504fd41459be3f2cd7ae485ac2ca343ba9c404
Visethboti <visethboti@gmail.com>
2023-02-09 15:26:57 +0700

    Before testing auth config

--- src/main/java/com/example/ProcessLeaveRequest/Config/SecurityConfig.java
+++ src/main/java/com/example/ProcessLeaveRequest/Config/SecurityConfig.java
@@ -2,17 +2,12 @@ package com.example.ProcessLeaveRequest.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -36,12 +31,9 @@ class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests().requestMatchers("/Employee/**").hasAnyRole("EMPLOYEE", "MANAGER").anyRequest()
-				.permitAll();
-		http.oauth2Login().and().logout().addLogoutHandler(keycloakLogoutHandler)
-				.logoutSuccessUrl("/successfullyLogout");
-		http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
-				.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
+		http.authorizeRequests().requestMatchers("/Employee/**").hasRole("EMPLOYEE").anyRequest().permitAll();
+		http.oauth2Login().and().logout().addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/");
+		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		return http.build();
 	}
 
@@ -50,14 +42,4 @@ class SecurityConfig {
 		return http.getSharedObject(AuthenticationManagerBuilder.class).build();
 	}
 
-	private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
-		JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-		jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
-		return jwtConverter;
-	}
-
-	@Bean
-	JwtDecoder jwtDecoder() {
-		return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-	}
 }

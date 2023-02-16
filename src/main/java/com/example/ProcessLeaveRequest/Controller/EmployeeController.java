9cc0a770766ba54aa29f0738768035a9e6c2adcb
Visethboti <visethboti@gmail.com>
2023-02-16 17:04:12 +0700

    Added employee review leave request

--- src/main/java/com/example/ProcessLeaveRequest/Controller/EmployeeController.java
+++ src/main/java/com/example/ProcessLeaveRequest/Controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.ProcessLeaveRequest.Controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.ProcessLeaveRequest.Entity.LeaveRequest;
 
 import io.camunda.tasklist.CamundaTaskListClient;
 import io.camunda.tasklist.auth.SelfManagedAuthentication;
+import io.camunda.tasklist.dto.Task;
 import io.camunda.tasklist.dto.TaskList;
 import io.camunda.tasklist.dto.TaskState;
+import io.camunda.tasklist.dto.Variable;
 import io.camunda.tasklist.exception.TaskListException;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
@@ -90,4 +94,66 @@ public class EmployeeController {
 		return "employee-request-leave-list";
 	}
 
+	@GetMapping("/ReviewLeaveRequest")
+	public String showReviewRequestLeave(Model theModel, @RequestParam("taskID") String taskID)
+			throws TaskListException {
+
+		// define variablies
+		LeaveRequest leaveRequest = new LeaveRequest();
+
+		// get task variables
+		SelfManagedAuthentication sma = new SelfManagedAuthentication().clientId("tasklist")
+				.clientSecret("XALaRPl5qwTEItdwCMiPS62nVpKs7dL7").keycloakUrl("http://localhost:18080")
+				.keycloakRealm("camunda-platform");
+
+		CamundaTaskListClient client = new CamundaTaskListClient.Builder().shouldReturnVariables()
+				.taskListUrl("http://localhost:8082/").authentication(sma).build();
+
+		Task task = client.getTask(taskID);
+		List<Variable> variables = task.getVariables();
+
+		for (Variable v : variables) {
+			System.out.println(v.getName() + ":" + v.getValue());
+			if (v.getName().equals("user_submitted"))
+				leaveRequest.setEmployeeUsername((String) v.getValue());
+			else if (v.getName().equals("field_startDate"))
+				leaveRequest.setStartDate((String) v.getValue());
+			else if (v.getName().equals("field_endDate"))
+				leaveRequest.setEndDate((String) v.getValue());
+			else if (v.getName().equals("field_comment"))
+				leaveRequest.setComment((String) v.getValue());
+
+		}
+
+		// add variablies to Model
+		theModel.addAttribute("leaveRequest", leaveRequest);
+		theModel.addAttribute("taskID", taskID);
+
+		return "employee-review-request-leave";
+	}
+
+	@PostMapping("/ProcessReviewRequestLeave")
+	public String processReviewLeaveRequest(@ModelAttribute("LeaveRequest") LeaveRequest leaveRequest,
+			@RequestParam("taskID") String taskID) throws TaskListException {
+
+		SelfManagedAuthentication sma = new SelfManagedAuthentication().clientId("tasklist")
+				.clientSecret("XALaRPl5qwTEItdwCMiPS62nVpKs7dL7").keycloakUrl("http://localhost:18080")
+				.keycloakRealm("camunda-platform");
+
+		CamundaTaskListClient client = new CamundaTaskListClient.Builder().shouldReturnVariables()
+				.taskListUrl("http://localhost:8082/").authentication(sma).build();
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		// Claim Task
+		client.claim(taskID, auth.getName());
+
+		// retrieve variables
+		Map<String, Object> map = Map.of("field_comment", leaveRequest.getComment());
+
+		// Finish Task
+		client.completeTask(taskID, map);
+
+		return "redirect:/Employee/RequestLeaveList";
+	}
 }
